65c8027f7c3bca9f754556771f8a0b8681d00afd
Артем <artem@melchakov>
2018-07-25 11:37:02 +0300

    Fix analytics controller

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,9 +1,7 @@
 class AnalyticsController < ApplicationController
   def create
-    file = File.new("params.txt", "a")
-    file.puts params
-    file.close
-  	respond_with AnalyticForm.new(analytic_params).save, location: analytics_path
+    attributes = params[:analytic].nil? ? simple_params : analytic_params
+  	respond_with AnalyticForm.new(attributes).save, location: analytics_path
   end
 
   def index
@@ -21,6 +19,10 @@ class AnalyticsController < ApplicationController
                                        :screenColors_GA, :screenResolution_GA, :siteSpeedSampleRate_GA, :storage_GA, :storeGac_GA, :storedClientId_GA, :trackingId_GA,
                                        :useAmpClientId_GA, :userId_GA, :viewportSize_GA, :XSRF_TOKEN_Cookie, :ga_Cookie, :ym_d_Cookie, :ym_uid_Cookie, 
                                        :laravel_session_Cookie, :date_Header, :user_agent_Header, :lang_Header,:mobile_phone)
+    end
+
+    def simple_params
+      params.except!(:controller, :action)
     end
 end
 
