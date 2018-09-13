0b5b098533a2992c9199c8ad12f32671e7ac5fe3
Артем <artem@melchakov>
2018-09-13 13:00:29 +0300

    Fix rails_db

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/home/deployer/dump"
 set :deploy_user, "deployer"
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", ".env", "config/rails_db.rb"
+append :linked_files, "config/database.yml", ".env", "config/initializers/rails_db.rb"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
